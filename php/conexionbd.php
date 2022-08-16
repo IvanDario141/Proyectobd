cacbc3dbcab28bf9acc3781c08cd2dcdebb46852
IvanDario141 <73681727+IvanDario141@users.noreply.github.com>
2022-08-16 12:58:03 -0500

    Update conexionbd.php
    Cambiar puerto

--- php/conexionbd.php
+++ php/conexionbd.php
@@ -6,7 +6,7 @@
     
     // Create connection
     
-    $conn = mysqli_connect($servername, $username, $password, $database, 3306);
+    $conn = mysqli_connect($servername, $username, $password, $database, 3307);
     if($conn -> connect_error){
         echo("Se produjo un error al conectar a la base de datos");
     }
